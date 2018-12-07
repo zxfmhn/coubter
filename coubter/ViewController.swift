41b9c3bed79c95995018b1c4978e3da152b8766f
zxdmhn <2861123614@qq.com>
2018-12-07 21:15:45 +0800

    计算器5
    学生作业

--- coubter/ViewController.swift
+++ coubter/ViewController.swift
@@ -8,327 +8,223 @@
 
 import UIKit
 class ViewController: UIViewController {
-    var operatorFlag: Double = 0
-    var temp: Double = 0
-    var value: Double = 0
-    var temp1: Double = 0
-    var displaytext: String = ""
-    func RemoveZero(testNumber:Double)->String{
-        var str = "\(testNumber)"
-        var str2 = str.components(separatedBy: ".")
-        if(str2[1]=="")
-        {
-            str2[1] = "0"
-        }
-        if(Int(str2[1]) == 0)
-        {
-            str = str2[0]
-        }
-        return str
-    }
-    @IBOutlet weak var display: UITextField!
+    var re: Double = 0
+    var number: Double = 0
+    var add: Double = 0
+    var judge: Double = 0
+    @IBOutlet weak var display1: UITextField!
+    @IBOutlet weak var display2: UITextField!
+    @IBOutlet weak var result: UILabel!
     @IBAction func buttonZeon(_ sender: Any) {
-        if(display.text == "0" )
-        {
-            display.text = ""
+        if re == 1{
+            result.text = "0"
+        }else{
+            result.text = result.text! + "0"
         }
-        if(display.text == "+" )
-        {
-            display.text = ""
-        }
-        if(display.text == "-" )
-        {
-            display.text = ""
-        }
-        if(display.text == "*" )
-        {
-            display.text = ""
-        }
-        if(display.text == "/" )
-        {
-            display.text = ""
-        }
-        display.text = display.text! + "0"
     }
     @IBAction func buttonOne(_ sender: Any) {
-    if(display.text == "0" )
-    {
-        display.text = ""
+        if re == 1{
+            result.text = "1"
+        }else{
+            result.text = result.text! + "1"
+        }
     }
-    if(display.text == "+" )
-    {
-    display.text = ""
-    }
-    if(display.text == "-" )
-    {
-    display.text = ""
-    }
-    if(display.text == "*" )
-    {
-    display.text = ""
-    }
-    if(display.text == "/" )
-    {
-    display.text = ""
-    }
-    display.text = display.text! + "1"
-}
 @IBAction func buttonTwo(_ sender: Any) {
-    if(display.text == "0" )
-    {
-        display.text = ""
+    if re == 1{
+        result.text = "2"
+    }else{
+        result.text = result.text! + "2"
     }
-    if(display.text == "+" )
-    {
-        display.text = ""
-    }
-    if(display.text == "-" )
-    {
-        display.text = ""
-    }
-    if(display.text == "*" )
-    {
-        display.text = ""
-    }
-    if(display.text == "/" )
-    {
-        display.text = ""
-    }
-    display.text = display.text! + "2"
 }
 @IBAction func buttonThree(_ sender: Any) {
-    if(display.text == "0" )
-    {
-        display.text = ""
+    if re == 1{
+        result.text = "3"
+    }else{
+        result.text = result.text! + "3"
     }
-    if(display.text == "+" )
-    {
-        display.text = ""
-    }
-    if(display.text == "-" )
-    {
-        display.text = ""
-    }
-    if(display.text == "*" )
-    {
-        display.text = ""
-    }
-    if(display.text == "/" )
-    {
-        display.text = ""
-    }
-    display.text = display.text! + "3"
 }
 @IBAction func buttonFour(_ sender: Any) {
-    if(display.text == "0" )
-    {
-        display.text = ""
+    if re == 1{
+        result.text = "4"
+    }else{
+        result.text = result.text! + "4"
     }
-    if(display.text == "+" )
-    {
-        display.text = ""
-    }
-    if(display.text == "-" )
-    {
-        display.text = ""
-    }
-    if(display.text == "*" )
-    {
-        display.text = ""
-    }
-    if(display.text == "/" )
-    {
-        display.text = ""
-    }
-    display.text = display.text! + "4"
 }
 @IBAction func buttonFive(_ sender: Any) {
-    if(display.text == "0" )
-    {
-        display.text = ""
+    if re == 1{
+        result.text = "5"
+    }else{
+        result.text = result.text! + "5"
     }
-    if(display.text == "+" )
-    {
-        display.text = ""
-    }
-    if(display.text == "-" )
-    {
-        display.text = ""
-    }
-    if(display.text == "*" )
-    {
-        display.text = ""
-    }
-    if(display.text == "/" )
-    {
-        display.text = ""
-    }
-    display.text = display.text! + "5"
 }
 @IBAction func buttonSix(_ sender: Any) {
-    if(display.text == "0" )
-    {
-        display.text = ""
+    if re == 1{
+        result.text = "6"
+    }else{
+        result.text = result.text! + "6"
     }
-    if(display.text == "+" )
-    {
-        display.text = ""
-    }
-    if(display.text == "-" )
-    {
-        display.text = ""
-    }
-    if(display.text == "*" )
-    {
-        display.text = ""
-    }
-    if(display.text == "/" )
-    {
-        display.text = ""
-    }
-    display.text = display.text! + "6"
 }
 @IBAction func buttonSeven(_ sender: Any) {
-    if(display.text == "0" )
-    {
-        display.text = ""
+    if re == 1{
+        result.text = "7"
+    }else{
+        result.text = result.text! + "7"
     }
-    if(display.text == "+" )
-    {
-        display.text = ""
-    }
-    if(display.text == "-" )
-    {
-        display.text = ""
-    }
-    if(display.text == "*" )
-    {
-        display.text = ""
-    }
-    if(display.text == "/" )
-    {
-        display.text = ""
-    }
-    display.text = display.text! + "7"
 }
 @IBAction func buttonEight(_ sender: Any) {
-    if(display.text == "0" )
-    {
-        display.text = ""
+    if re == 1{
+        result.text = "8"
+    }else{
+        result.text = result.text! + "8"
     }
-    if(display.text == "+" )
-    {
-        display.text = ""
-    }
-    if(display.text == "-" )
-    {
-        display.text = ""
-    }
-    if(display.text == "*" )
-    {
-        display.text = ""
-    }
-    if(display.text == "/" )
-    {
-        display.text = ""
-    }
-    display.text = display.text! + "8"
 }
 @IBAction func buttonNine(_ sender: Any) {
-    if(display.text == "0" )
-    {
-        display.text = ""
+    if re == 1{
+        result.text = "9"
+    }else{
+        result.text = result.text! + "9"
     }
-    if(display.text == "+" )
-    {
-        display.text = ""
-    }
-    if(display.text == "-" )
-    {
-        display.text = ""
-    }
-    if(display.text == "*" )
-    {
-        display.text = ""
-    }
-    if(display.text == "/" )
-    {
-        display.text = ""
-    }
-    display.text = display.text! + "9"
 }
 @IBAction func bottonDian(_ sender: Any) {
-    display.text = display.text! + "."
-    display.text = display.text! + ""
+    result.text = result.text! + "."
+    judge = 1
     
 }
-@IBAction func buttonAdd(_ sender: Any) {
-    operatorFlag=1
-    temp = Double (display.text!)!
-    display.text = "+"
+    
+    @IBAction func percent(_ sender: Any) {
+        let count = Double(result.text!)!
+        let count2 = count * 0.01
+        result.text = String(count2)
+        re = 0
+    }
+    @IBAction func buttonAdd(_ sender: Any) {
+    if add == 1{
+        let number1 = Double(display1.text!)!
+        let number2 = Double(result.text!)!
+        let number3 = number1 + number2
+        display1.text = String(number3)
+        result.text = ""
+        number = 2
+        re = 1
+    }else{
+        if result.text == ""{
+            result.text = "0"
+        }else{
+            let x = Double(result.text!)
+            display1.text = String(x!)
+            result.text = ""
+            number = 2
+            re = 0
+        }
+    }
     
 }
-    @IBAction func buttonSlow(_ sender: Any){
-    operatorFlag=2
-    temp = Double(display.text!)!
-    display.text = "-"
-    
+    @IBAction func buttonminus(_ sender: Any){
+        if add == 1{
+            let number1 = Double(display1.text!)!
+            let number2 = Double(result.text!)!
+            let number3 = number1 - number2
+            display1.text = String(number3)
+            result.text = ""
+            number = 2
+            re = 1
+        }else{
+            if result.text == ""{
+                result.text = "0"
+            }else{
+                let x = Double(result.text!)
+                display1.text = String(x!)
+                result.text = ""
+                number = 2
+                re = 0
+            }
+        }
 }
-@IBAction func buttonChu(_ sender: Any) {
-    operatorFlag=3
-    temp = Double(display.text!)!
-    display.text = "/"
+@IBAction func buttonDivisor(_ sender: Any) {
+    if add == 1{
+        let number1 = Double(display1.text!)!
+        let number2 = Double(result.text!)!
+        let number3 = number1 / number2
+        display1.text = String(number3)
+        result.text = ""
+        number = 2
+        re = 1
+    }else{
+        if result.text == ""{
+            result.text = "0"
+        }else{
+            let x = Double(result.text!)
+            display1.text = String(x!)
+            result.text = ""
+            number = 2
+            re = 0
+        }
+    }
     
 }
 @IBAction func buttonProduct(_ sender: Any) {
-    operatorFlag=4
-    temp = Double (display.text!)!
-    display.text = "*"
+    if add == 1{
+        let number1 = Double(display1.text!)!
+        let number2 = Double(result.text!)!
+        let number3 = number1 * number2
+        display1.text = String(number3)
+        result.text = ""
+        number = 2
+        re = 1
+    }else{
+        if result.text == ""{
+            result.text = "0"
+        }else{
+            let x = Double(result.text!)
+            display1.text = String(x!)
+            result.text = ""
+            number = 2
+            re = 0
+        }
+    }
     
 }
     @IBAction func buttonResult(_ sender: Any) {
-    if   operatorFlag == 1  {
-        temp1 = temp + Double(display.text!)!
-        display.text = "\(temp1)"
-        if(!display.text!.contains("+")){
-        temp = temp1 + Double(display.text!)!
-        display.text = "\(temp)"
+        var number4: Double
+        var number5: Double
+        let x = Double(display1.text!)!
+        number5 = (result.text! as NSString).doubleValue
+        if number == 1{
+            number4 = x - number5
+        }else if number == 2{
+            number4 = x + number5
+        }else if number == 3{
+            number4 = x * number5
+        }else if number == 4{
+            number4 = x / (number5)
+        }else {
+            number4 = 1000
         }
-        
-    }
-    if   operatorFlag == 2  {
-        temp = temp - Double(display.text!)!
-        value = temp
-        display.text = "\(temp)"
-        
-    }
-    if  operatorFlag == 3 {
-        temp = temp / Double(display.text!)!
-        value = temp
-        display.text = "\(temp)"
-        
-    }
-    if   operatorFlag == 4{
-        temp = temp * Double(display.text!)!
-        value = temp
-        display.text = "\(temp)"
-        
-    }
+        display2.text = String(number5)
+        if judge == 1{
+            result.text = String(format:"%f",number4)
+        }else {
+            result.text = String(format:"%.0f",number4)
+        }
+        re = 1
+        judge = 0
+        add = 0
+
 }
 @IBAction func buttonReset(_ sender: Any) {
-    display.text = "0"
+    result.text = ""
 }
 
 
 @IBAction func DeleteNumber(_ sender: Any) {
-    if(display.text!.count == 1)
+    if(result.text!.count == 1)
     {
-        display.text = "0"
-        displaytext = ""
+        result.text = "0"
+        result.text = ""
     }
-    if(display.text != "0"){
-        display.text!.remove(at: display.text!.index(before:display.text!.endIndex))
-        displaytext = display.text!
+    if(result.text != "0"){
+        result.text!.remove(at: result.text!.index(before:result.text!.endIndex))
+        result.text = result.text!
     }
 }
 
